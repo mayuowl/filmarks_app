074a1a6e66d85c4bad26c5d34577f844599a8536
廣田真由 <my-hrt@i.softbank.jp>
2018-10-27 10:52:55 +0900

    add end and timestamps for user_movie_star migration

--- db/migrate/20181025124914_user_movie_star.rb
+++ db/migrate/20181025124914_user_movie_star.rb
@@ -2,6 +2,8 @@ class UserMovieStar < ActiveRecord::Migration[5.2]
   def change
     create_table :user_movie_clips do |t|
       t.references :user
-      t.references :movie    
+      t.references :movie 
+      t.timestamps
+    end       
   end
 end
