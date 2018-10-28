6c2d7844ffe22deeef14c689e017f5423d33284d
廣田真由 <my-hrt@i.softbank.jp>
2018-10-28 17:36:04 +0900

    be annotate

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id         :bigint(8)        not null, primary key
+#  user_id    :bigint(8)
+#  comment    :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Review < ApplicationRecord
 end
