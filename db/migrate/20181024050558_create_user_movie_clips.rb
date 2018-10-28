a1c45f6f8a64002bd0f6d6475ac6abe9990ca2a9
廣田真由 <my-hrt@i.softbank.jp>
2018-10-28 21:54:00 +0900

    change name create_user_movie_clips migration

---
+++ db/migrate/20181024050558_create_user_movie_clips.rb
@@ -0,0 +1,9 @@
+class CreateUserMovieClips < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_movie_clips do |t|
+      t.references :user
+      t.references :movie
+      t.timestamps
+    end
+  end
+end
