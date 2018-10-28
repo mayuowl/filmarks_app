e181c60e2e1798356e21634b77e75adea8c306b7
廣田真由 <my-hrt@i.softbank.jp>
2018-10-28 17:44:50 +0900

    be annotate

--- app/models/user_review_like.rb
+++ app/models/user_review_like.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: user_review_likes
+#
+#  id         :bigint(8)        not null, primary key
+#  user_id    :bigint(8)
+#  review_id  :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class UserReviewLike < ApplicationRecord
   belongs_to :review
   belongs_to :user
