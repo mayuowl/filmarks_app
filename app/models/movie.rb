ed4acf3bfcc877f0e9402b2572cd551b9236f100
廣田真由 <my-hrt@i.softbank.jp>
2018-10-28 16:25:05 +0900

    be annotate

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Movie < ApplicationRecord
   validates :title, presence: true
   has_many :user_movie_marks, dependent: :destroy
--- app/models/user_movie_mark.rb
+++ app/models/user_movie_mark.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: user_movie_marks
+#
+#  id         :bigint(8)        not null, primary key
+#  user_id    :bigint(8)
+#  movie_id   :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class UserMovieMark < ApplicationRecord
   belongs_to :user
   belongs_to :movie
