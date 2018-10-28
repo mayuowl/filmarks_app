f3316bcd4eea2d99ece04a75177cde528e7f5bde
廣田真由 <my-hrt@i.softbank.jp>
2018-10-28 22:00:41 +0900

    be annotate

--- app/models/movie_director.rb
+++ app/models/movie_director.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: movie_directors
+#
+#  id         :bigint(8)        not null, primary key
+#  movie_id   :bigint(8)
+#  cast_id    :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class MovieDirector < ApplicationRecord
   belongs_to :movie
   belongs_to :cast
