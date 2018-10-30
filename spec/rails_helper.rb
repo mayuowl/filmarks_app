b17eae42816ae30109b1d403c1824ad1c7ec8eb4
廣田真由 <my-hrt@i.softbank.jp>
2018-10-30 20:52:55 +0900

    set rspec setting

---
+++ spec/rails_helper.rb
@@ -0,0 +1,21 @@
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('../../config/environment', __FILE__)
+require 'spec_helper'
+require 'rspec/rails'
+require 'database_cleaner'
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+ActiveRecord::Migration.maintain_test_schema!
+DatabaseCleaner.strategy = :truncation
+Faker::Config.locale = :en
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
