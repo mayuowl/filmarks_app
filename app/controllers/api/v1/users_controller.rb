da5578136c7a4bbc7fb891bb5bbe69731daac58d
廣田真由 <my-hrt@i.softbank.jp>
2018-11-20 23:06:22 +0900

    fix routes and movies_controller users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
     render json: { status: 200, data: @users }
