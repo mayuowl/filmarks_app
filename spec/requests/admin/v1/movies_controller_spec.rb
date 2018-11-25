e4d9453f859daf2f1556235882cd0875cbe1c723
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2018-11-25 16:35:28 +0900

    Add update movie API and spec

--- spec/requests/admin/v1/movies_controller_spec.rb
+++ spec/requests/admin/v1/movies_controller_spec.rb
@@ -4,7 +4,7 @@ describe Admin::V1::MoviesController, type: :request do
   describe "create" do
     subject { post(admin_v1_movies_path, params: params) }
 
-    context "there are some data" do
+    context "new data" do
       let(:params) { { movie: { title: title } } }
       let(:title) { "test_title" }
 
@@ -15,6 +15,26 @@ describe Admin::V1::MoviesController, type: :request do
           expect(json["status"]).to eq 204
           movie = Movie.last
           expect(movie.title).to eq title
+        end
+      end
+    end
+  end
+
+  describe "update" do
+    subject { put(admin_v1_movie_path(movie.id), params: params) }
+
+    context "a data" do
+      let!(:movie) { create(:movie) }
+      let(:params) { { movie: { title: title } } }
+      let(:title) { Faker::Cat.name }
+
+      it do
+        aggregate_failures do
+          subject
+          json = JSON.parse(response.body)
+          expect(json["status"]).to eq 200
+          _data = json["data"]
+          expect(_data["title"]).to eq title
         end
       end
     end
