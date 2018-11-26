cb566292c858b9a6dee932e40df53ab27a302d61
廣田真由 <my-hrt@i.softbank.jp>
2018-11-27 00:57:37 +0900

    fix admin_movies_controller spec

--- spec/requests/admin/v1/movies_controller_spec.rb
+++ spec/requests/admin/v1/movies_controller_spec.rb
@@ -44,16 +44,16 @@ describe Admin::V1::MoviesController, type: :request do
     subject { post(admin_v1_movies_path, params: params) }
 
     context "destroy data" do
+      let!(:movie) { create(:movie) }
       let(:params) { { movie: { title: title } } }
       let(:title) { "test_title" }
 
       it do
         aggregate_failures do
+          binding.pry
           expect { subject }.to change { Movie.count }.by(-1)
           json = JSON.parse(response.body)
           expect(json["status"]).to eq 204
-          movie = Movie.last
-          expect(movie.title).to eq title
         end
       end
     end
